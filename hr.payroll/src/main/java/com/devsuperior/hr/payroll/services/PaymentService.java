f3daa55a0dd5802e9457e23a3d1f699ee106531b
Ruan-fe <55806119+Ruan-fe@users.noreply.github.com>
2021-01-16 12:04:12 -0300

    Feign Client

--- hr.payroll/src/main/java/com/devsuperior/hr/payroll/services/PaymentService.java
+++ hr.payroll/src/main/java/com/devsuperior/hr/payroll/services/PaymentService.java
@@ -1,13 +1,22 @@
 package com.devsuperior.hr.payroll.services;
 
 import com.devsuperior.hr.payroll.entities.Payment;
+import com.devsuperior.hr.payroll.entities.Worker;
+import com.devsuperior.hr.payroll.feignclients.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+
     public Payment getPayment(Long workerId, Integer days){
-            return new Payment("Bob",100.0, days);
+
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+
+        return new Payment(worker.getName(),worker.getDailyIncome(), days);
     }
 
 }
